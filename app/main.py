1fe25bd735f66f9a5f502725389125aa0d3bda38
yochimonji <yochimonji@gmail.com>
2022-09-13 17:51:31 +0000

    add: パスパラメータ

--- app/main.py
+++ app/main.py
@@ -10,6 +10,11 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/countries/{country_name}")
+async def country(country_name: str):
+    return {"country_name": country_name}
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
