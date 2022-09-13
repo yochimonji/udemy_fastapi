d6f8daf06ad634feb106f8be1f506fe3d9c76078
yochimonji <yochimonji@gmail.com>
2022-09-13 18:15:40 +0000

    add: リクエストボディ

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: int
+    tax: float
 
 
 @app.get("/")
@@ -15,6 +23,12 @@ async def country(country_name: str, country_no: int, city_name: Optional[str] =
     return {"country_name": country_name, "country_no": country_no}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
+
+
+@app.post("/item/")
+async def create_item(item: Item):
+
+    return {"message": f"{item.name}は税込み価格{item.price * item.tax}円になります。"}
