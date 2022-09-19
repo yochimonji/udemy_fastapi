ad26fa5d34188e39c095023a223077d0f5732acd
yochimonji <yochimonji@gmail.com>
2022-09-19 14:26:17 +0000

    add: databaseの定義を追加

---
+++ app/chapter7/sql_app/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    user_id = Column(Integer, primary_key=True, index=True)
+    user_name = Column(String, unique=True, index=True)
+
+
+class Room(Base):
+    __tablename__ = "rooms"
+    room_id = Column(Integer, primary_key=True, index=True)
+    room_name = Column(String, unique=True, index=True)
+    capacity = Column(Integer)
+
+
+class Booking(Base):
+    __tablename__ = "bookings"
+    booking_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.user_id", ondelete="SET NULL"), nullable=False)
+    room_id = Column(Integer, ForeignKey("rooms.room_id", ondelete="SET NULL"), nullable=False)
+    booked_num = Column(Integer)
+    start_datetime = Column(DateTime, nullable=False)
+    end_datetime = Column(DateTime, nullable=False)
